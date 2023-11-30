67c04c3268d05d332a1f20f6eb36fac1ca09373a
Ohleesang <jammmmed@gmail.com>
2023-11-30 17:47:12 +0900

    최대한 효율적으로 짠거같은데 이래도 메모리초과..

--- app/src/main/java/com/example/a87/MainActivity.kt
+++ app/src/main/java/com/example/a87/MainActivity.kt
@@ -11,40 +11,57 @@ import java.util.Arrays
 
 /**
  * n = 3  [0,0] = 1
- * 1 2 3  [0,1],[1,0] [1,1] = 2                                 0 1 2
- * 2 2 3  [0,2],[1,2],[2,2],[2,1],[2,0] = 3                     3 4 5
- * 3 3 3  [0,3],[1,3],[2,3],[3,3],[3,2],[3,1],[3,0] = 4         6 7 8
+ * 1 2 3  [0,1],[1,0] [1,1] = 2
+ * 2 2 3  [0,2],[1,2],[2,2],[2,1],[2,0] = 3
+ * 3 3 3  [0,3],[1,3],[2,3],[3,3],[3,2],[3,1],[3,0] = 4
  *
  * n = 4
- * 1 2 3 4                                                      0 1 2 3
- * 2 2 3 4                                                      4 5 6 7
- * 3 3 3 4                                                      8 9 a b
- * 4 4 4 4                                                      c d e f
+ * 1 2 3 4
+ * 2 2 3 4
+ * 3 3 3 4
+ * 4 4 4 4
  *
  *
  * [0,n-1]...[n-1,n-1]...[n-1,0] = n 이 채워진다
  * n 의 count = 2n-1
  *
- * n= 3 일때,
- * 1 -> 0
- * 2 -> 1 ,3,4
- * 3 -> 2 ,5,6,7,8
- * n = 4 일때
- * 1 -> 0
- * 2 -> 1 ,4,5
- * 3 -> 2 ,6, 8,9,10
- * 4 -> 3 ,7, 11,12,13,14,15
+ *  n = 1
+ *  1
+ *  n = 2
+ *  1 2 / 2 2
+ *  n = 3
+ *  1 2 3 / 2 2 3 / 3 3 3
+ *  n = 4
+ *  1 2 3 4 / 2 2 3 4 / 3 3 3 4 / 4 4 4 4
+ *  n = 5
+ *  1 2 3 4 5 / 2 2 3 4 5 / 3 3 3 4 5 / 4 4 4 4 5 / 5 5 5 5 5
  */
 class Solution {
     fun solution(n: Int, left: Long, right: Long): IntArray {
         var answer = intArrayOf()
         var arrList = mutableListOf<Int>()
+        var tempList = mutableListOf<Int>()
         //2차원 배열 만들기 <= x 시간복잡도 많이 잡아먹는듯
+        var count = 1
+        var index = 0
+        loop@ for(i in 1..n){
+            for(j in 1..n){
+                if(count>=j)
+                    arrList.add(count)
+                else
+                    arrList.add(j)
 
-        /// n -> (n-1)^2~n^2-1 까지 n이 채워져 있음
-        var range = ((n-1)*(n-1)..n*n-1)
-
-
+                if(left<=index && index<=right){
+                    tempList.add(arrList.last())
+                }
+                else if(index>right) {
+                    break@loop
+                }
+                index++
+            }
+            count++
+        }
+        answer = tempList.toIntArray()
         return answer
     }
 }
